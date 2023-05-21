7b3e47beb4a8e3cfba2bddd27041fa4d680d26c6
Erkan Bostan <profesyonel59@gmail.com>
2023-05-21 08:43:40 +0300

    HubContext Transactions Completed.

---
+++ SignalRUI.Core/Controllers/NotificationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRUI.Core.Hubs;
+
+namespace SignalRUI.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> _hubContext;
+
+        public NotificationController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            MyHub.TeamCount = teamCount;
+            await _hubContext.Clients.All.SendAsync("Notify",$"Arkadaşlar Takımlar {teamCount} Kişilik Oluşturulacaktır.");
+            return Ok();
+        }
+    }
+}
--- SignalRUI.Core/Hubs/MyHub.cs
+++ SignalRUI.Core/Hubs/MyHub.cs
@@ -4,15 +4,38 @@ namespace SignalRUI.Core.Hubs
 {
     public class MyHub : Hub
     {
-        public static List<string> Names { get; set; } = new List<string>();
+        private static List<string> Names { get; set; } = new List<string>();
+        private static int ClientCount { get; set; } = 0;
+        public static int TeamCount { get; set; } = 3;
         public async Task SendName(string name)
         {
-            Names.Add(name);
-            await Clients.All.SendAsync("ReceiveName", name);
+            if (Names.Count > TeamCount)
+            {
+                await Clients.Caller.SendAsync("Error", $"Takım En Fazla {TeamCount} Kişi Olabilir");
+            }
+            else
+            {
+                Names.Add(name);
+                await Clients.All.SendAsync("ReceiveName", name);
+            }
+               
         }
         public async Task GetNames()
         {
             await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnConnectedAsync();
+        }
+        public async override Task OnDisconnectedAsync(Exception? exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
